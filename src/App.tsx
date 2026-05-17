cf8b458d3edd04e53e8392581f3edd3a4a8eb11b
hetzel401 <max.hetzel@icloud.com>
2026-05-17 17:37:00 +0300

    aaah

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
-import { Route, Routes, Router as WouterRouter } from "wouter";
 import { HelmetProvider } from "react-helmet-async";
 import { LanguageProvider } from "@/context/LanguageContext";
 import { CustomizationProvider } from "@/context/CustomizationContext";
 import { LanyardProvider } from "@/context/LanyardContext";
 import Index from "@/pages/Index";
-import DashboardPage from "@/pages/DashboardPage";
-import NotFound from "@/pages/not-found";
 import CustomCursor from "@/components/CustomCursor";
 import BackgroundManager from "@/components/BackgroundManager";
 import EffectsLayer from "@/components/EffectsLayer";
@@ -22,23 +19,12 @@ import LoadingScreen from "@/components/LoadingScreen";
 import AnnouncementBanner from "@/components/AnnouncementBanner";
 import LiveFavicon from "@/components/LiveFavicon";
 
-function Router() {
-  return (
-    <Routes>
-      <Route path="/" component={Index} />
-      <Route path="/admin" component={DashboardPage} />
-      <Route path="*" component={NotFound} />
-    </Routes>
-  );
-}
-
 function App() {
   return (
     <HelmetProvider>
       <LanguageProvider>
         <CustomizationProvider>
           <LanyardProvider>
-          <WouterRouter base={import.meta.env.BASE_URL.replace(/\/$/, "")}>
             <div className="windows-shell">
               <LiveFavicon />
               <LoadingScreen />
@@ -50,14 +36,13 @@ function App() {
               <EffectsLayer />
               <WindowsNav />
               <CommandPalette />
-              <Router />
+              <Index />
               <NowPlayingWidget />
               <StatusBanner />
               <ClockWidget />
               <QuoteWidget />
               <BackToTop />
             </div>
-          </WouterRouter>
           </LanyardProvider>
         </CustomizationProvider>
       </LanguageProvider>
